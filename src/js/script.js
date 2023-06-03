616042f425840e14a1f0ee56b072ec727f5062a8
Jacek <jacek_135@yahoo.pl>
2023-06-03 19:07:04 +0200

    updatet and finished js

--- src/js/script.js
+++ src/js/script.js
@@ -5,22 +5,20 @@ const addNewBtn = document.querySelector('.recipes-add-btn')
 const submitBtn = document.querySelector('.submit-btn')
 const closePopupBtn = document.querySelector('.popup-cancel')
 const recipesBox = document.querySelector('.recipes-box')
+const error = document.querySelector('.popup-error')
 
 const allInputs = document.querySelectorAll('.popup-input')
 const foodNameInput = document.querySelector('#food-name')
 const foodIngredientsInput = document.querySelector('#ingredients')
 const foodDescInput = document.querySelector('#food-description')
 
-///Default and saved food
-localStorage.setItem('defaultArrayKey', JSON.stringify([]))
-
-const arrToStorage = JSON.parse(localStorage.getItem('defaultArrayKey'))
-const defaultFood1 = ['spagetti', 'pasa,meat,sauce', 'cook Pasta, fry meat and mix all with sauce. Enyoy!']
-
-arrToStorage.push(defaultFood1)
-localStorage.setItem('arrayKey', JSON.stringify(arrToStorage))
-
-console.log(JSON.parse(localStorage.getItem('arrayKey')));
+const defaultFood = ['spagetti', 'pasa,meat,sauce', 'cook Pasta, fry meat and mix all with sauce. Enyoy!']
+const defaultFood2 = [
+	'Scrumbled Eggs',
+	'Eggs, butter, bread, sausage',
+	'fry Eggs and cutted sausage on butter - Eat with bread',
+]
+const arrToStorage = JSON.parse(localStorage.getItem('arrayKey'))
 
 ///LOAD MAIN Values
 
@@ -45,28 +43,48 @@ const checkSubmit = () => {
 
 	if (checkInputs(ok) && !editing) {
 		handlePopup()
+		memoUserItem(newFoodName, newFoodIngredients, newFoodDesc)
 		createRecipe(newFoodName, newFoodIngredients, newFoodDesc)
 		clearPopupInputs()
+		error.classList.add('hide')
 	} else if (checkInputs(ok) && editing) {
 		handlePopup()
 		makeChanges(newFoodName, newFoodIngredients, newFoodDesc)
 		clearPopupInputs()
+		error.classList.add('hide')
 		editing = false
 	} else {
-		console.log('czegoś brakuje')
+		error.classList.remove('hide')
+		// console.log('czegoś brakuje')
 	}
 }
 const handleExtension = e => {
+	const openBlock = document.querySelector('.extended')
 	const parentBlock = e.target.parentElement.parentElement
-	parentBlock.classList.toggle('extended')
+
+	if (openBlock === null && !parentBlock.classList.contains('extended')) {
+		parentBlock.classList.add('extended')
+	} else if (openBlock !== null && !parentBlock.classList.contains('extended')) {
+		openBlock.classList.remove('extended')
+		parentBlock.classList.add('extended')
+	} else {
+		parentBlock.classList.remove('extended')
+	}
 }
 const removeRecipe = e => {
 	const parentToRemove = e.target.parentElement.parentElement.parentElement
+	const indexOfItem = checkNumOfItem(parentToRemove)
 
+	memoUserDelete(indexOfItem)
 	parentToRemove.remove()
 }
 const clearPopupInputs = () => {
 	allInputs.forEach(input => (input.value = ''))
+}
+
+const checkNumOfItem = item => {
+	const itemsBlock = item.parentElement
+	return Array.from(itemsBlock.children).indexOf(item) - 2
 }
 
 /// MAIN Program Modules
@@ -118,17 +136,54 @@ const makeChanges = (newFoodName, newFoodIngredients, newFoodDesc) => {
 	const recipeName = elementToEdit.querySelector('.recipe-name')
 	const recipeIngredients = elementToEdit.querySelector('.recipe-ingredients')
 	const recipeDescription = elementToEdit.querySelector('.recipe-introduction-text')
+	const indexOfItem = checkNumOfItem(elementToEdit)
 
 	recipeName.textContent = newFoodName
 	recipeIngredients.textContent = newFoodIngredients
 	recipeDescription.firstElementChild.innerText = newFoodDesc
 
 	elementToEdit.classList.remove('element-in-edition')
+	memoUserChanges(indexOfItem, newFoodName, newFoodIngredients, newFoodDesc)
 }
+
+/// Local Storage Management
+
+const checkNewUser = () => {
+	if (arrToStorage === null) {
+		const arr = []
+		arr.push(defaultFood)
+		arr.push(defaultFood2)
+		localStorage.setItem('arrayKey', JSON.stringify(arr))
+		// console.log(`...zaktualizowałem localhost`)
+		createRecipe(defaultFood[0], defaultFood[1], defaultFood[2])
+		createRecipe(defaultFood2[0], defaultFood2[1], defaultFood2[2])
+	} else {
+		for (let i = 0; i < arrToStorage.length; i++) {
+			createRecipe(arrToStorage[i][0], arrToStorage[i][1], arrToStorage[i][2])
+		}
+		// console.log(`${arrToStorage} - w localhost już ok`)
+	}
+}
+const memoUserItem = (name, ingredients, description) => {
+	const newArr = [name, ingredients, description]
+	arrToStorage.push(newArr)
+	localStorage.setItem('arrayKey', JSON.stringify(arrToStorage))
+}
+const memoUserDelete = index => {
+	arrToStorage.splice(index, 1)
+	localStorage.setItem('arrayKey', JSON.stringify(arrToStorage))
+}
+const memoUserChanges = (index, newFoodName, newFoodIngredients, newFoodDesc) => {
+	arrToStorage[index] = [newFoodName, newFoodIngredients, newFoodDesc]
+	localStorage.setItem('arrayKey', JSON.stringify(arrToStorage))
+}
+
+//addUserItem('test','składniki testowe','przykładowy opis')
 
 ///LOAD DOM Events
 
 addNewBtn.addEventListener('click', handlePopup)
 submitBtn.addEventListener('click', checkSubmit)
 closePopupBtn.addEventListener('click', handlePopup)
-createRecipe(defaultFood1[0], defaultFood1[1], defaultFood1[2]);
+checkNewUser()
+// updateUserData(arrToStorage)
